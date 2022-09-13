c86ba0b96213d91f21d1c8670c39f4a29ebbc17d
Megha <meghabmugali@gmail.com>
2022-09-13 20:58:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.assignment1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private int mCount = 0;
+    private TextView mShowCount;
+    boolean check = false;
+    private Button btnzero;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = findViewById(R.id.textView);
+        btnzero = findViewById(R.id.btnzero);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, "Hello Toast", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null) {
+            mShowCount.setText(Integer.toString(mCount));
+        }
+    }
+
+    public void onClick(View arg0) {
+        check = true;
+        btnzero.setBackgroundColor(Color.BLUE);
+
+        if (check == true) {
+
+            btnzero.setBackgroundColor(Color.MAGENTA);
+        }
+    }
+}
+
+
